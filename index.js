e83e9a3b69441561d26bad00e6d50efe5f281aaf
Karen Byrd <klbdev88@gmail.com>
2024-01-18 15:35:53 -0600

    javaScript set buttons to read on phone display, set time lapse from display of phone to pager display, reset btn works

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // javascript
+
+let pagerDisplay = document.getElementById('pager-display');
+let phoneDisplay = document.getElementById('phone-display');
+let resetBtn = document.getElementById('reset');
+const sendBtn = document.getElementById('send');
+
+
+const buttons = document.querySelectorAll('.phone-button');
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        const totalNumbers = 0
+        if(totalNumbers <= 10){
+            let number = e.target.getAttribute('data-number');
+            phoneDisplay.innerHTML += number
+        } else {
+            reset()
+        }
+        console.log(totalNumbers)
+        })
+    })
+
+resetBtn.addEventListener('click', reset);
+
+function reset() {
+    pagerDisplay.innerHTML = '';
+    phoneDisplay.innerHTML = '';
+}
+
+
+
+send.addEventListener('click', () => {
+    setTimeout(() => {
+        pagerDisplay.innerHTML = phoneDisplay.innerHTML;
+        phoneDisplay.innerHTML = '';
+    }, 1500);
+})
+
